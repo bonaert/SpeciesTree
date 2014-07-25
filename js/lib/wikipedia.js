a13eccca118a1e28175df58c1ed24c2ae9edfe4f
harokb <harokb@gmail.com>
2014-07-25 18:19:50 +0100

    Simplified and refactored entire module.

--- js/lib/wikipedia.js
+++ js/lib/wikipedia.js
@@ -1,140 +1,110 @@
+/*jslint browser: true, devel: true, nomen: true */
 function Wikipedia() {
     var self = this;
-    this.url_article = 'http://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exlimit=10&redirects=&titles='
-    this.url_image = "http://en.wikipedia.org/w/api.php?action=query&redirects&generator=images&gimlimit=20&prop=imageinfo&iiprop=url|dimensions|mime&format=json&titles="
+    this.url_article = 'http://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exlimit=10&redirects=&titles=';
+    this.url_image = "http://en.wikipedia.org/w/api.php?action=query&redirects&generator=images&gimlimit=20&prop=imageinfo&iiprop=url|dimensions|mime&format=json&titles=";
+
 
     this.article = function (commonName, speciesName, onSuccess) {
-        if (typeof commonName === "undefined") {
-            this._simple_article(speciesName, onSuccess);
-            return;
+        var names = [commonName, speciesName];
+        var urls = self._build_urls(names, self._makeArticleUrl);
+        self._makeRequestUntilGoodContent(urls, self.processArticle, self._hasGoodContent, onSuccess);
+    };
+
+    this._makeArticleUrl = function (name) {
+        return encodeURI(self.url_article + name + '&callback=?');
+    };
+
+    this._hasGoodContent = function (result) {
+        if (_.isUndefined(result) || _.isUndefined(result.extract)) {
+            // No content
+            return false;
+        } else if (result.extract.indexOf('may refer') !== -1) {
+            // Content is a disambiguation page
+            return false;
+        } else {
+            return true;
         }
+    };
 
-        var url = self._make_article_url(commonName);
+    this.processArticle = function (data) {
+        var pages = data.query.pages;
+        var content_keys = self._getContentKeys(pages);
 
-        this._makeRequest(url, function (data) {
-            var result = self.process_article(data);
-
-            if (typeof result === "undefined") {
-                self._simple_article(speciesName, onSuccess);
-                return;
-            } else if (typeof result.extract === "undefined") {
-                self._simple_article(speciesName, onSuccess);
-                return;
-            } else if (result.extract.indexOf('may refer') !== -1) {
-                self._simple_article(speciesName, onSuccess);
-                return;
-            }
-
-            onSuccess(result);
-        });
-    }
-
-    this._simple_article = function (name, onSuccess) {
-        var url = self._make_article_url(name);
-        self._makeRequest(url, function (data) {
-            onSuccess(self.process_article(data));
-        });
-    }
-
-    this._make_article_url = function (name) {
-        return encodeURI(this.url_article + name + '&callback=?');
-    }
-
-    this._get_useful_keys = function (result) {
-        var keys = [];
-        for (var key in result) {
-
-            // Bad key, usually present when there was no corresponding text
-            if (key === -1) {
-                continue;
-            }
-
-            // Strangely, sometimes there is a result, but no text associated
-            // Maybe due to an error of the Wikipedia Api Extract add on
-            // Therefore we exclude it
-            if (typeof result[key].extract === "undefined") {
-                continue;
-            }
-
-            keys.push(key);
-        };
-        return keys;
-    }
-
-    this.process_article = function (data) {
-        var result = data.query.pages;
-        var keys = self._get_useful_keys(result);
-
-        if (keys.length === 0 && (typeof result[-1] !== "undefined") && (result[-1].missing === "")) {
+        if (self._isNoContent(content_keys, pages)) {
             console.info("No result for article.");
             return undefined;
         }
 
-        return result[keys[0]];
-    }
+        return pages[content_keys[0]];
+    };
+
+    this._getContentKeys = function (result) {
+        var keys = _.keys(result);
+        var useful_keys = _.filter(keys, function (key) {
+            return (key !== -1) && _.isDefined(result[key].extract);
+        });
+        return useful_keys;
+    };
+
+    this._isNoContent = function (content_keys, pages) {
+        return content_keys.length === 0 && _.isDefined(pages[-1]) && (pages[-1].missing === "");
+    };
 
     this.image = function (commonName, speciesName, onSuccess) {
-        if (typeof commonName === "undefined") {
-            this._simple_image(speciesName, onSuccess);
-            return;
-        }
+        var names = [commonName, speciesName];
+        var urls = self._build_urls(names, self._makeImageUrl);
+        self._makeRequestUntilGoodContent(urls, self.process_image, self._imageHasGoodContent, onSuccess);
+    };
 
-        var url = self._make_image_url(commonName);
+    this._imageHasGoodContent = function (result) {
+        return _.isDefined(result);
+    };
 
-        this._makeRequest(url, function (data) {
-            var result = self.process_image(data);
-
-            if (typeof result === "undefined") {
-                self._simple_image(speciesName, onSuccess);
-                return;
-            }
-
-            onSuccess(result);
-        });
-    }
-
-    this._make_image_url = function (name) {
-        return encodeURI(this.url_image + name + '&callback=?');
-    }
-
-    this._simple_image = function (name, onSuccess) {
-        var url = self._make_image_url(name);
-        self._makeRequest(url, function (data) {
-            onSuccess(self.process_image(data));
-        });
-    }
-
-    this._get_image_keys = function (result) {
-        var keys = [];
-        for (var key in result) {
-            keys.push(key);
-        }
-        return keys;
-    }
+    this._makeImageUrl = function (name) {
+        return encodeURI(self.url_image + name + '&callback=?');
+    };
 
     this.process_image = function (data) {
-        console.log(data);
-        if (typeof data.query === "undefined") {
-            return undefined;
-        } else if (typeof data.query.pages === "undefined") {
+        if (_.isUndefined(data.query) || _.isUndefined(data.query.pages)) {
             return undefined;
         }
 
         var result = data.query.pages;
-        var keys = self._get_image_keys(result).sort(function (a, b) {
+        var keys = _.keys(result).sort(function (a, b) {
             return a - b;
         });
-        var imagesData = [];
-        for (var i = 0; i < keys.length; i++) {
-            var key = keys[i];
-            imagesData.push(result[key]);
-        };
+
+        var imagesData = _.map(keys, function (key) {
+            return result[key];
+        });
 
         return imagesData;
-    }
+    };
 
+    this._makeRequestUntilGoodContent = function (urls, process_data, has_good_content, onSuccess) {
+        if (urls.length === 0) {
+            onSuccess(undefined);
+            return;
+        }
+
+        var url = urls[0];
+        this._makeRequest(url, function (data) {
+            var result = process_data(data);
+            if (has_good_content(result)) {
+                onSuccess(result);
+            } else {
+                self._makeRequestUntilGoodContent(_.tail(urls), onSuccess);
+            }
+        });
+    };
+
+    this._build_urls = function (names, make_url) {
+        var defined_names = _.filter(names, _.isDefined);
+        return _.map(defined_names, make_url);
+    };
 
     this._makeRequest = function (url, onSuccess) {
         $.getJSON(url, onSuccess);
-    }
+    };
 }
