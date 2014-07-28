fc70bd2237c275550d3940e88ec9fcaf78db943e
harokb <harokb@gmail.com>
2014-07-28 18:48:29 +0100

    Added handler for sitemap.

--- speciesexplorer.py
+++ speciesexplorer.py
@@ -8,7 +8,12 @@ class GoogleWebmasterVerifier(webapp2.RequestHandler):
     def get(self):
         self.response.write(file('google7e0693b4ccda33f7.html').read())
 
+class GoogleWebmasterVerifier(webapp2.RequestHandler):
+    def get(self):
+        self.response.write(file('sitemap.xml').read())
+
 application = webapp2.WSGIApplication([
     ('/google7e0693b4ccda33f7.html', GoogleWebmasterVerifier),
+    ('/sitemap.xml', SiteMapHandler),
     ('/', MainPage)
 ], debug=True)
