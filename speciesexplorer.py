8c850e79d3c249645f0e6f836c0587860d3736ac
harokb <harokb@gmail.com>
2014-07-29 17:47:31 +0100

    Added handler for about page and index.html.

--- speciesexplorer.py
+++ speciesexplorer.py
@@ -13,8 +13,14 @@ class SiteMapHandler(webapp2.RequestHandler):
         self.response.headers["Content-Type"] = "application/xml"
         self.response.write(file('sitemap.xml').read())
 
+class AboutHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write(file('about.html').read())
+
 application = webapp2.WSGIApplication([
     ('/google7e0693b4ccda33f7.html', GoogleWebmasterVerifier),
     ('/sitemap.xml', SiteMapHandler),
+    ('/about.html', AboutHandler),
+    ('/index.html', MainPage),
     ('/', MainPage)
 ], debug=True)
