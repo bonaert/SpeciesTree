b3e611a4a88b2aa8e6619821e86a48c9f61c5c7a
harokb <harokb@gmail.com>
2014-08-01 18:16:37 +0100

    Added constans for the common animals we want to fetch.

---
+++ constants.py
@@ -0,0 +1,70 @@
+BIRD = [212, 4]
+REPTILE = [358, 4]
+
+MOLLUSCS = [52, 4]
+CHEPHALOPODS = [136, 5]
+
+MAMMAL = [359, 4]
+
+RODENTS = [1459, 5]
+
+FISSIPEDIA = [732, 5]
+MUSTELIDAE = [5307, 6]  # badgers, weasels, martens, ferrets, minks and wolverines
+CANIDAE = [9701, 6]  # domestic dogs, wolves, foxes, jackals, coyotes,
+CANIS = [5219142, 7]  # Dogs and wolves
+VULPES = [5219234, 7]  # Foxes
+FELIDAE = [9703, 6]  # Cats
+FELIS = [2435022, 7]  # Familiar domestic cat and its closest wild relatives
+LEOPARDUS = [2434918, 7]  # Familiar domestic cat and its closest wild relatives
+PANTHERA = [2435194, 7]  # Familiar domestic cat and its closest wild relatives
+
+PRIMATES = [798, 5]
+OLD_WORLD_MONKEYS = [9622, 6]
+MAN_LIKE_PRIMATES = [5483, 6]
+HOMINOIDS = [2436435, 7]
+
+DOLPHIN_WHALES = [733, 5]
+
+ODD_TOED_UNGULATES = [795, 5]
+EQUIDAE = [795, 6]
+
+AMPHISBAENIANS = [715, 5]  # Snakes, geckos, lizards
+
+RAY_FINNED_FISHES = [204, 5]
+CARTILAGENOUS_FISHES = [121, 5]
+
+INSECTS = [216, 4]
+ARACHNIDS = [367, 4]
+
+CNIDARIANS = [43, 4]  # Jellyfish
+
+BEINGS = [
+    BIRD,
+    REPTILE,
+    MOLLUSCS,
+    CHEPHALOPODS,
+    MAMMAL,
+    RODENTS,
+    FISSIPEDIA,
+    MUSTELIDAE,
+    CANIDAE,
+    CANIS,
+    VULPES,
+    FELIDAE,
+    FELIS,
+    LEOPARDUS,
+    PANTHERA,
+    PRIMATES,
+    OLD_WORLD_MONKEYS,
+    MAN_LIKE_PRIMATES,
+    HOMINOIDS,
+    DOLPHIN_WHALES,
+    ODD_TOED_UNGULATES,
+    EQUIDAE,
+    AMPHISBAENIANS,
+    RAY_FINNED_FISHES,
+    CARTILAGENOUS_FISHES,
+    INSECTS,
+    ARACHNIDS,
+    CNIDARIANS
+]
