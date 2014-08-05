339cb505a43e004cc29d9de885c41c84faccc212
harokb <harokb@gmail.com>
2014-08-05 23:33:09 +0100

    When doing a search, simply search, i.e. query = name. Can populate any node's children, given an ID. Stylistic fixes.

--- speciesexplorer.py
+++ speciesexplorer.py
@@ -24,11 +24,11 @@ from populate import Organism
 ranks = ['KINGDOM', 'PHYLUM', 'CLASS', 'ORDER', 'FAMILY', 'GENUS', 'SPECIES']
 INDEX = search.Index(name="organisms")
 
-filenames = set(['about.html', 'index.html', 'sitemap.xml', 'google7e0693b4ccda33f7.html'])
+file_names = {'about.html', 'index.html', 'sitemap.xml', 'google7e0693b4ccda33f7.html'}
 
 
 def get_file(filename):
-    if filename in filenames:
+    if filename in file_names:
         return get_from_cache(filename)
     else:
         return get_from_cache('index.html')
@@ -47,39 +47,61 @@ def get_from_cache(filename):
 class PopulateHandler(webapp2.RequestHandler):
     def get(self):
         if self.request.get('admin') == credentials.ADMIN_KEY:
-            taskqueue.add(url='/populate', params={'admin': credentials.ADMIN_KEY})
+            params = {'admin': credentials.ADMIN_KEY}
+            if self.request.get('id'):
+                params['id'] = self.request.get('id')
+
+            if self.request.get('rank_id'):
+                params['rank_id'] = self.request.get('rank_id')
+
+            taskqueue.add(url='/populate', params=params)
             self.response.write('Added task!')
         else:
             self.response.write('Wrong key!')
 
     def post(self):
-        if self.request.get('admin') == credentials.ADMIN_KEY:
-            time1 = time.time()
+        if self.request.get('admin') != credentials.ADMIN_KEY:
+            return
+
+        ID = self.request.get('id')
+        rank_id = self.request.get('rank_id')
+        if ID != "" and rank_id != "":
+            try:
+                ID = int(ID)
+                rank_id = int(rank_id)
+                populator = populate.Populator(self.request.host_url)
+                populator.populate_id(ID, rank_id)
+                return
+            except ValueError:
+                pass
+        else:
             populator = populate.Populator(self.request.host_url)
             populator.populate()
-            time2 = time.time()
-            logging.info('It took %s s to populate the datastore.' % (time2 - time1))
 
 
-def deleteData():
+def delete_data_in_datastore():
     ndb.delete_multi(
         Organism.query().iter(keys_only=True)
     )
 
 
-def delete_all_in_index(index_name):
+def delete_data_in_index(index_name):
     """Delete all the docs in the given index."""
     doc_index = search.Index(name=index_name)
 
     # looping because get_range by default returns up to 100 documents at a time
     while True:
         # Get a list of documents populating only the doc_id field and extract the ids.
-        document_ids = [document.doc_id
-                        for document in doc_index.get_range(ids_only=True, limit=100)]
+        document_ids = [doc.doc_id for doc in doc_index.get_range(ids_only=True, limit=100)]
         if not document_ids:
             break
         # Delete the documents for the given ids from the Index.
         doc_index.delete(document_ids)
+
+
+def delete_data():
+    delete_data_in_datastore()
+    delete_data_in_index('organisms')
 
 
 class DeleteDataHandler(webapp2.RequestHandler):
@@ -92,16 +114,12 @@ class DeleteDataHandler(webapp2.RequestHandler):
 
     def post(self):
         if self.request.get('admin') == credentials.ADMIN_KEY:
-            time1 = time.time()
-            deleteData()
-            delete_all_in_index('organisms')
-            time2 = time.time()
-            logging.info('It took %s s to delete the data.' % (time2 - time1))
+            delete_data()
 
 
 class MainHandler(webapp2.RequestHandler):
     def get(self, filename):
-        if filename in filenames:
+        if filename in file_names:
             if filename == 'sitemap.xml':
                 self.response.headers["Content-Type"] = "application/xml"
             self.response.write(get_file(filename))
@@ -131,81 +149,52 @@ def organism_to_dict(organism):
     return result
 
 
-def build_json(organisms):
+def build_json_representation(organisms):
     organism_dict = [organism_to_dict(organism) for organism in organisms if organism]
     return json.dumps(organism_dict)
 
 
 def get_organism(ID):
-    organism = Organism.query(Organism.ID == ID).get()
-    logging.info(organism)
-    return organism
+    return Organism.query(Organism.ID == ID).get()
 
 
 def get_data_from_id(ID):
     organism = get_organism(ID)
-    return build_json([organism])
+    return build_json_representation([organism])
 
 
 def do_search(search_q):
     logging.info(search_q)
-    sortopts = search.SortOptions(expressions=[
-        search.SortExpression(expression='Organism.ID', direction='ASCENDING', default_value=9999999999999999)])
     search_query = search.Query(
         query_string=search_q,
         options=search.QueryOptions(
-            sort_options=sortopts,
             limit=10))
     search_results = INDEX.search(search_query)
     return search_results
 
 
 def get_organisms(search_results):
-    organisms = []
-    for doc in search_results:
-        ID = int(doc.doc_id)
-        logging.info(ID)
-        organisms.append(get_organism(ID))
-    return organisms
+    ids = sorted([int(doc.doc_id) for doc in search_results])
+    return [get_organism(ID) for ID in ids]
 
 
 def get_search_results_for_name(name):
-    # for rank in ranks:
-    vernacular_name_query = 'vernacularName = ' + name  # + ' AND rank=' + rank
-    results = do_search(vernacular_name_query)
-
+    results = do_search(name)
     if results.number_found != 0:
         return results
-
-    general_query = 'canonicalName = %s OR scientificName = %s' % (name, name)
-    #general_query = '((canonicalName = %s OR scientificName = %s) AND rank=%s)' % (name, name, rank)
-    results = do_search(general_query)
-
-    if results.number_found != 0:
-        return results
-
-    return None
+    else:
+        return None
 
 
 def get_search_results(name):
-    search_results = get_search_results_for_name(name)
-
-    if not search_results and len(name) > 4:
-        search_results = get_search_results_for_name(name[:-1])
-
-    if not search_results and len(name) > 5:
-        search_results = get_search_results_for_name(name[:-2])
-
-    return search_results
+    return get_search_results_for_name(name)
 
 
 def get_data_from_name(name):
     search_results = get_search_results(name)
     if search_results:
-        logging.info(len(search_results.results))
-        logging.info(search_results.results)
         organisms = get_organisms(search_results)
-        return build_json(organisms)
+        return build_json_representation(organisms)
     else:
         return '[]'
 
@@ -218,7 +207,7 @@ class GetDataHandler(webapp2.RequestHandler):
         elif self.request.get('name'):
             result = get_data_from_name(self.request.get('name'))
         else:
-            result = {}
+            result = '{"error": "You must pass an id or a name"}'
 
         self.response.headers["Content-Type"] = "application/json"
         self.response.write(result)
